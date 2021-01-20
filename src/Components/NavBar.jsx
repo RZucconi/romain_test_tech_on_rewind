9ee33b263ef390760b2d584c0aa5ef724549535c
RZucconi <romain.zucconi@gmail.com>
2021-01-20 22:48:28 +0100

    NavBar component is now functional and responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,3 +1,25 @@
+import { Link } from "react-router-dom";
+
+import Logo from "../Images/logo_test_tech.PNG";
+
+import "../Styles/NavBar.css";
+
+const links = [
+  { path: "/funzone", name: "Funzone" },
+  { path: "/testimoniales", name: "Testimoniales" },
+];
+
 export default function NavBar() {
-  return <h1>NavBar</h1>;
+  return (
+    <header className="headerNav">
+      <Link to="/">
+        <img src={Logo} alt="webapp logo" width="100px" />
+      </Link>
+      <nav className="navMenu">
+        {links.map((link) => (
+          <Link to={link.path}>{link.name}</Link>
+        ))}
+      </nav>
+    </header>
+  );
 }
