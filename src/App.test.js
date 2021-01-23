e67366f6215e1b3e711962c1610a34f720d03a05
RZucconi <romain.zucconi@gmail.com>
2021-01-23 22:11:06 +0100

    fix font title font color

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import Layout from "./Layout/Layout";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(<Layout />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
