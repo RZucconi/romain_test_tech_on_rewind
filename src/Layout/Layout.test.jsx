9e809be587b4525e05bc38f274e14196d2dec203
RZucconi <romain.zucconi@gmail.com>
2021-01-24 17:13:45 +0100

    try to use jest for testing app

--- src/Layout/Layout.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Layout from "./Layout";
-
-it("retrieves data from the Routes file and uses it to create routes components in which the SingleVideo or Thumbnails component is displayed", () => {
-  render(<Layout routes={{ routes }} />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
