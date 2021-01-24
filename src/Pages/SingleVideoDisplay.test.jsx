2853b67410def2de0d39fb1a9e222362a254db7c
RZucconi <romain.zucconi@gmail.com>
2021-01-24 18:24:05 +0100

    delete unnecessary type on graphAl query & try to use MockedProvider on test without expected success

--- src/Pages/SingleVideoDisplay.test.jsx
+++ src/Pages/SingleVideoDisplay.test.jsx
@@ -3,7 +3,27 @@ import { MockedProvider } from "@apollo/client/testing";
 import SingleVideoDisplay from "./SingleVideoDisplay";
 import { VIDEO } from "../Components/Query";
 
-const mocks = [];
+const mocks = [
+  {
+    request: {
+      query: VIDEO,
+      variables: {
+        id: "100cd810-9dc9-4346-bcfc-6382dfe99b9b",
+      },
+    },
+    result: {
+      data: {
+        video: {
+          name: "Martine à la plage",
+          url: "http://martine.com/picture/martinealaplage.png",
+          Tag: {
+            name: "Lecture jeunesse",
+          },
+        },
+      },
+    },
+  },
+];
 
 it("renders without error", () => {
   const component = TestRenderer.create(
